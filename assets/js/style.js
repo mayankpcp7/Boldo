a0bb95becc03570198cd26009bd6593c4213bc9d
mayankpcp7 <jariwalmayank123@gmail.com>
2024-01-10 11:42:38 +0530

    merge code

--- assets/js/style.js
+++ assets/js/style.js
@@ -6,3 +6,22 @@ function closeNav() {
   document.getElementById("Menu").style.left = "-100%";
   document.getElementsByTagName("body")[0].style.overflow = "scroll";
 }
+const counters = document.querySelectorAll(".value");
+const speed = 600;
+
+counters.forEach((counter) => {
+  const animate = () => {
+    const value = +counter.getAttribute("akhi");
+    const data = +counter.innerText;
+
+    const time = value / speed;
+    if (data < value) {
+      counter.innerText = Math.ceil(data + time);
+      setTimeout(animate, 1);
+    } else {
+      counter.innerText = value;
+    }
+  };
+
+  animate();
+});
