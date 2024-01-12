d0867148cda19e821a00f13c82b4352ef397c2c7
mayankpcp7 <jariwalmayank123@gmail.com>
2024-01-12 11:56:31 +0530

    last push

--- assets/js/style.js
+++ assets/js/style.js
@@ -25,18 +25,38 @@ counters.forEach((counter) => {
 
   animate();
 });
-// -----back to top------
+
 
 // ========== BACK TO TOP============
-const backToTopButton = document.getElementById("back-to-top");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    backToTopButton.style.display = "block";
+// const backToTopButton = document.getElementById("back-to-top");
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 200) {
+//     backToTopButton.style.display = "block";
+//   } else {
+//     backToTopButton.style.display = "none";
+//   }
+// });
+
+// backToTopButton.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// });
+let backToTopButton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backToTopButton.style.display = "flex";
   } else {
     backToTopButton.style.display = "none";
   }
-});
+}
 
+// When the user clicks on the button, scroll to the top of the document
 backToTopButton.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
